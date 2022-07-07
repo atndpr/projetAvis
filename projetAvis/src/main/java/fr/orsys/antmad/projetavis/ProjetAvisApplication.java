a8d2a5552e9b428e2a5a5e1241f0fdd65fd96ff5
Anthony Delpierre <anthony.delpierre@icloud.com>
2022-07-07 11:18:24 +0200

    Adds a Maven Project

---
+++ projetAvis/src/main/java/fr/orsys/antmad/projetavis/ProjetAvisApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.antmad.projetavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAvisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAvisApplication.class, args);
+    }
+
+}
---
+++ projetAvis/src/test/java/fr/orsys/antmad/projetavis/ProjetAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orsys.antmad.projetavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAvisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
